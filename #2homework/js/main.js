4031445b65a06102a4a305a529489b4e62ee665a
Dima <dmytrohensetsiy@gmail.com>
2020-11-13 16:40:28 +0200

    hw#2

--- #2homework/js/main.js
+++ #2homework/js/main.js
@@ -1,39 +1,39 @@
-
-let getMinNumber;
-let getMaxNumber;
-
-do {
-    getMinNumber = +prompt("Введіть, будь ласка, мінімальне значення", "0");
-    getMinNumber = +getMinNumber.toFixed(0);
-
-} while (isNaN(getMinNumber));
-
+let minNumber;
+let maxNumber;
 
 do {
-    getMaxNumber = +prompt("Введіть, будь ласка, максимальне значення", "100");
-    getMaxNumber = +getMaxNumber.toFixed(0);
+    minNumber = +prompt("Введіть, будь ласка, мінімальне значення", "0");
+    minNumber = +minNumber.toFixed(0);
 
-    while (getMinNumber > getMaxNumber || getMaxNumber === null) {
-        getMaxNumber = +prompt("Введіть, будь ласка, максимальне значення, яке більше мінімального");
-        getMaxNumber = +getMaxNumber.toFixed(0);
+} while (isNaN(minNumber));
+
+
+do {
+    maxNumber = +prompt("Введіть, будь ласка, максимальне значення", "100");
+    maxNumber = +maxNumber.toFixed(0);
+
+
+    while (maxNumber <= minNumber && maxNumber !== 0) {
+        maxNumber = +prompt("Введіть, будь ласка, максимальне значення, яке більше мінімального");
+        maxNumber = +maxNumber.toFixed(0);
     };
 
-} while (isNaN(getMinNumber));
+} while (isNaN(maxNumber));
 
 let odd = confirm("Чи потрібно пропускати парні числа?");
 let result = 0;
 
-for (let i = getMinNumber; i < getMaxNumber + 1; i++) {
+for (let i = minNumber; i < maxNumber + 1; i++) {
     if (odd === false) {
         result += i;
     } else if (odd === true) {
         i % 2 === 0 ? 0 : result += i;
-    }
-}
+    };
+};
 
 console.log(`
-Мінімальне значення: ${getMinNumber}
-Маскимальне значення: ${getMaxNumber}
+Мінімальне значення: ${minNumber}
+Маскимальне значення: ${maxNumber}
 Чи потрібно пропускати парні числа?: ${odd}
 Сума чисел: ${result}
 `);
